16e1f1448964d3a89fb010fb16e227829b0777b6
YukkuriC <799433638@qq.com>
2022-06-12 18:19:49 +0800

    init

---
+++ HarmonyMansus/HarmonyMansus.cs
@@ -0,0 +1,10 @@
+﻿using SecretHistories.Constants.Modding;
+using HarmonyLib;
+
+public class HarmonyMansus
+{
+    public static void Initialise(ISecretHistoriesMod mod)
+    {
+        Harmony.CreateAndPatchAll(typeof(HarmonyMansus));
+    }
+}
