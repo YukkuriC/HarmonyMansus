9ffef43deaf82c334ae0506d145f317f04131a5c
Yukkuri C <799433638@qq.com>
2022-06-13 14:13:34 +0800

    basic speedup function
    TODO: crash on exit
    TODO: configurable speed-up factor

--- HarmonyMansus/ModMain.cs
+++ HarmonyMansus/ModMain.cs
@@ -3,4 +3,10 @@
 [HarmonyPatch]
 public class ModMain
 {
+    [HarmonyPrefix, HarmonyPatch(typeof(Heart), "Beat")]
+    public static void AlteredBeatStep(ref float seconds, ref float metaseconds)
+    {
+        if (seconds == 0 || seconds == metaseconds) return;
+        seconds = 1; // TODO: read config
+    }
 }
