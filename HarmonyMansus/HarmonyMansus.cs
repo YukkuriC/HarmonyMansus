c301f87840ead456cc35e694aadcd0a3db17a4b7
YukkuriC <799433638@qq.com>
2022-06-13 14:10:48 +0800

    auto unpatch & patching try catch

--- HarmonyMansus/HarmonyMansus.cs
+++ HarmonyMansus/HarmonyMansus.cs
@@ -1,11 +1,27 @@
 ﻿using SecretHistories.Constants.Modding;
 using HarmonyLib;
+using System;
 
+[HarmonyPatch]
 public class HarmonyMansus
 {
+    public static Harmony patcher;
     public static void Initialise(ISecretHistoriesMod mod)
     {
-        var patcher = new Harmony("yukkuric." + mod.Id);
-        patcher.PatchAll();
+        patcher = new Harmony("yukkuric." + mod.Id);
+        try
+        {
+            patcher.PatchAll();
+        }
+        catch (Exception e)
+        {
+            NoonUtility.LogException(e);
+        }
+    }
+
+    [HarmonyPrefix, HarmonyPatch(typeof(MenuScreenController), "Exit")]
+    public static void ExitBeforeCrash()
+    {
+        patcher.UnpatchAll();
     }
 }
