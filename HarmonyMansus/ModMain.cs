af07e532d740e56b2f7a60095fa64fb7084461ba
Yukkuri C <799433638@qq.com>
2022-06-13 23:53:24 +0800

    init

--- HarmonyMansus/ModMain.cs
+++ HarmonyMansus/ModMain.cs
@@ -1,6 +1,14 @@
 ﻿using HarmonyLib;
+using SecretHistories.Constants.Modding;
 
 [HarmonyPatch]
-public class ModMain
+public class TheMod
 {
+    public static void Initialise(ISecretHistoriesMod mod) => HarmonyMansus.Patch(mod);
+
+    [HarmonyPostfix,HarmonyPatch(typeof(Heart),"Beat")]
+    public static void test(float seconds,float metaseconds)
+    {
+        NoonUtility.Log(string.Format("BEAT: {0},{1}", seconds, metaseconds));
+    }
 }
